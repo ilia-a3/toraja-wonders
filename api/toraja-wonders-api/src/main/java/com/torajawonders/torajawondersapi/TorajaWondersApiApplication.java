3394aa4fc0200e55f7632144c0842ec86e117dec
cs <cs.ia1387@gmail.com>
2023-09-21 08:32:14 +1000

    Initialised Spring Boot API Layer

---
+++ api/toraja-wonders-api/src/main/java/com/torajawonders/torajawondersapi/TorajaWondersApiApplication.java
@@ -0,0 +1,13 @@
+package com.torajawonders.torajawondersapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TorajaWondersApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TorajaWondersApiApplication.class, args);
+	}
+
+}
